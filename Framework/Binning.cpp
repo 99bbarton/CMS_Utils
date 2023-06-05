d19aaab58951a6510c637a33c279e5921c3ac97f
Ben Barton <99bbarton@gmail.com>
2023-06-05 14:19:33 +0200

    Correct this. to this->

--- Framework/Binning.cpp
+++ Framework/Binning.cpp
@@ -1,4 +1,3 @@
-
 //Implementation of Binning class for storing information on histogram bin limits and labelling
 
 #include "Binning.h"
@@ -7,28 +6,28 @@
 //Copy constructor
 Binning::Binning(Binning& that)
 {
-    this.name = that.name;
-    this.nBins = that.nBins;
-    this.isCustom = that.isCustom;
-    this.min = that.min;
-    this.max = that.max;
+    this->name = that.name;
+    this->nBins = that.nBins;
+    this->isCustom = that.isCustom;
+    this->min = that.min;
+    this->max = that.max;
     if (that.lowEdges != NULL)
     {
-        double lowEdges[this.nBins + 1];
-        double labels[this.nBins];
+        double lowEdges[this->nBins + 1];
+        double labels[this->nBins];
         for (int i = 0; i < nBins; i++)
         {
             lowEdges[i] = that.lowEdges[i];
             labels[i] = that.labels[i];
         }
-        lowEdges[this.nBins+1] = that.lowEdges[this.nBins+1];
-        this.lowEdges = lowEdges;
-        this.labels = labels;
+        lowEdges[this->nBins+1] = that.lowEdges[this->nBins+1];
+        this->lowEdges = lowEdges;
+        this->labels = labels;
     }
     else
     {
-        this.lowEdges = NULL;
-        this.labels = NULL;
+        this->lowEdges = NULL;
+        this->labels = NULL;
     }
     
 };
@@ -36,28 +35,28 @@ Binning::Binning(Binning& that)
 //Non-custom binning constructor
 Binning::Binning(string name, int nBins, double min, double max)
 {
-    this.name = name;
-    this.nBins = nBins;
-    this.min = min;
-    this.max = max;
-    this.isCustom = false;
-    this.lowEdges = NULL;
-    this.labels = NULL;
+    this->name = name;
+    this->nBins = nBins;
+    this->min = min;
+    this->max = max;
+    this->isCustom = false;
+    this->lowEdges = NULL;
+    this->labels = NULL;
 }
 
 //Custom binning constructor
 Binning::Binning(string name, int nBins, double lowEdges[], string labels[])
 {
-    this.name = name;
-    this.nBins = nBins;
-    this.isCustom = true;
+    this->name = name;
+    this->nBins = nBins;
+    this->isCustom = true;
 
     if (lowEdges != NULL)
     {
-        this.min = lowEdges[0];
-        this.max = lowEdges[nBins+1]; 
-        this.lowEdges = LowEdges;
-        this.labels = Labels;
+        this->min = lowEdges[0];
+        this->max = lowEdges[nBins+1]; 
+        this->lowEdges = LowEdges;
+        this->labels = Labels;
     }
     else
     {
